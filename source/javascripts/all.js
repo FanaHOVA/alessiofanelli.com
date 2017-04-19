2549e29a40c8faec60c35c2f135dd5c0753baa69
Alessio Fanelli <fanahova@gmail.com>
2017-04-19 18:58:50 +0100

    Add fade in of columns

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,18 @@
 //= require_tree .
+//= require 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+
+document.addEventListener('DOMContentLoaded', function(){
+    setTimeout(function() {
+      $(".sections-row").fadeIn(200);
+    }, 2800);
+    Typed.new('.hello', {
+      strings: ["Hello."],
+      typeSpeed: 95
+    });
+
+    Typed.new('.interests', {
+      strings: ["What are you interested in?"],
+      startDelay: 1100,
+      typeSpeed: 15
+    });
+});
